f694551fff47255e8a7c40472d3909ff2f1e834c
winniewambui <winniebwambui7@gmail.com>
2023-07-07 00:31:22 +0300

    fix: bugs in js functionality

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
+// const questions = document.querySelectorAll('#question');
+
+// questions.forEach(questionContainer => {
+//     const answer = questionContainer.querySelector('.question-text');
+//     const button = questionContainer.querySelector('.quiz-btn');
+
+//     button.addEventListener('click', function () {
+//         answer.classList.toggle('active');
+//         button.querySelector('.plus-icon').classList.add('hide');
+//         button.querySelector('.minus-icon').classList.add('show');
+
+//     });
+// });
+
+// const questions = document.querySelectorAll('#question');
+
+
 const questions = document.querySelectorAll('#question');
 
 questions.forEach(questionContainer => {
-    const answer = questionContainer.querySelector('.question-text');
-    const button = questionContainer.querySelector('.quiz-btn');
+  const answer = questionContainer.querySelector('.question-text');
+  const button = questionContainer.querySelector('.quiz-btn');
+  const plusIcon = button.querySelector('.plus-icon');
+  const minusIcon = button.querySelector('.minus-icon');
 
-    button.addEventListener('click', function () {
-        answer.classList.toggle('active');
-        button.querySelector('.plus-icon').classList.add('hide');
-        button.querySelector('.minus-icon').classList.add('show');
+  button.addEventListener('click', function () {
+    const isActive = answer.classList.contains('active');
+
+    questions.forEach(question => {
+      const questionAnswer = question.querySelector('.question-text');
+      const questionButton = question.querySelector('.quiz-btn');
+      const questionPlusIcon = questionButton.querySelector('.plus-icon');
+      const questionMinusIcon = questionButton.querySelector('.minus-icon');
+
+      questionAnswer.classList.remove('active');
+      questionPlusIcon.classList.remove('hide');
+      questionMinusIcon.classList.remove('show');
     });
+
+    if (!isActive) {
+      answer.classList.add('active');
+      plusIcon.classList.add('hide');
+      minusIcon.classList.add('show');
+    }
+  });
 });
-
-
